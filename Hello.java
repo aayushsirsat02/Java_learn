a4aea5f5bb7b7b45e137b45dec03e47938298c5c
aayushcodes-dev <sirsataayush4@gmail.com>
2025-07-13 02:23:05 +0530

    java datatypes , literals, and variables

---
+++ Hello.java
@@ -0,0 +1,29 @@
+public class Hello {
+    public static void main(String[] args) {
+        
+
+        //integer variables :
+
+        int a = 10; // 4 bytes
+        long b = 20L; // 8 bytes
+        short c = 30; // 2 bytes
+        byte d = 40; // 1 byte
+
+        //floating point variables :
+        float e = 10.5f; // 4 bytes
+        double f = 20.5; // 8 bytes
+        //character variable :
+        char g = 'A'; // 2 bytes
+        //boolean variable :
+        boolean h = true; // 1 bit (not precisely defined in terms of bytes)
+        //string variable :
+        String i = "Hello, World!"; // reference type, not a primitive
+        //printing the variables
+        System.out.println("Integer a: " + a);
+        System.out.println("Long b: " + b);
+        System.out.println("Short c: " + c);
+
+
+
+    }
+}
